482db3a9e137bc2e77151eb814bcdeedc0078f33
jhossymar <jhossymar@chrisjcom.jhossy>
2017-04-06 17:55:27 +0000

    se eliminaron espacios

--- logout.php
+++ logout.php
@@ -1,7 +1,7 @@
 <?php 
- $past = time() - 100; 
+ $past = time() - 3600; 
  //this makes the time in the past to destroy the cookie 
- setcookie(ID_my_site, gone, $past); 
- setcookie(Key_my_site, gone, $past); 
+ setcookie('ID_my_site', "", $past); 
+ setcookie('Key_my_site', "", $past); 
  header("Location: login.php"); 
  ?> 
