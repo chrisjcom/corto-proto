6e40ea2de7438a2885ae06ba8564552dda794bbb
Jhossymar Contreras <chrisj_contreras@outlook.com>
2017-04-07 00:51:53 +0000

    usuarios agregados hardcode

--- login.php
+++ login.php
@@ -1,17 +1,28 @@
 <?php 
+include 'users.php';
 //Connects to your Database 
-$conect = mysqli_connect("localhost","root","210591", "simple-php-login") or die(mysql_error()); 
+//$conect = mysqli_connect("localhost","root","210591", "simple-php-login") or die(mysql_error()); 
 //Checks if there is a login cookie
 if(isset($_COOKIE['ID_your_site'])){ //if there is, it logs you in and directes you to the members page
- 	$username = $_COOKIE['ID_your_site']; 
+ 	$username = $_COOKIE['ID_your_site'];
  	$pass = $_COOKIE['Key_your_site'];
- 	$check = mysqli_query($conect, "SELECT * FROM users WHERE username = '$username'")or die(mysql_error());
+	$usuario = getUser($username);
+	//foreach ($users as $key => $value) {		
+		if($username == $usuario['user']) {			
+			if($pass == $usuario['pass']) {				
+				header("Location: members.php");
+			} else {
+				header("Location: login.php");
+			}
+		}	
+	//}	
+ 	/*$check = mysqli_query($conect, "SELECT * FROM users WHERE username = '$username'")or die(mysql_error());
  	while($info = mysqli_fetch_array( $check )){
  		if ($pass != $info['password']){}
  		else{
  			header("Location: login.php");
 		}
- 	}
+ 	}*/
  }
  //if the login form is submitted 
  if (isset($_POST['submit'])) {
@@ -23,21 +34,39 @@ if(isset($_COOKIE['ID_your_site'])){ //if there is, it logs you in and directes 
  		die('You did not fill in a password.');
  	}
  	// checks it against the database
- 	if (!get_magic_quotes_gpc()){
+ 	/*if (!get_magic_quotes_gpc()){
  		$_POST['email'] = addslashes($_POST['email']);
- 	}
- 	$check = mysqli_query($conect, "SELECT * FROM users WHERE username = '".$_POST['username']."'")or die(mysql_error());
+ 	}*/
+ 	//$check = mysqli_query($conect, "SELECT * FROM users WHERE username = '".$_POST['username']."'")or die(mysql_error());
  //Gives error if user dosen't exist
- $check2 = mysqli_num_rows($check);
+ /*$check2 = mysqli_num_rows($check);
  if ($check2 == 0){
 	die('That user does not exist in our database.<br /><br />If you think this is wrong <a href="login.php">try again</a>.');
-}
-while($info = mysqli_fetch_array( $check )){
-	$_POST['pass'] = stripslashes($_POST['pass']);
- 	$info['password'] = stripslashes($info['password']);
- 	$_POST['pass'] = md5($_POST['pass']);
+}*/	
+	$usuario = getUser($_POST['username']);
+	//foreach ($users as $key => $value) {		
+		if($_POST['username'] = $usuario['user']) {			
+			$_POST['pass'] = stripslashes($_POST['pass']);
+ 			$usuario['pass'] = stripslashes($usuario['pass']);
+			if ($_POST['pass'] != $usuario['pass']){
+				die('Incorrect password, please <a href="login.php">try again</a>.');
+			}
+			else{ // if login is ok then we add a cookie 
+				$_POST['username'] = stripslashes($_POST['username']); 
+				$hour = time() + 3600; 
+				setcookie('ID_your_site', $_POST['username'], $hour); 
+				setcookie('Key_your_site', $_POST['pass'], $hour);	 
+				//then redirect them to the members area 
+				header("Location: members.php"); 
+			}
+		}
+	//}
+	//while($info = mysqli_fetch_array( $check )){
+	//$_POST['pass'] = stripslashes($_POST['pass']);
+ 	//$info['password'] = stripslashes($info['password']);
+ 	//$_POST['pass'] = md5($_POST['pass']);
 	//gives error if the password is wrong
- 	if ($_POST['pass'] != $info['password']){
+ 	/*if ($_POST['pass'] != $info['password']){
  		die('Incorrect password, please <a href="login.php">try again</a>.');
  	}
 	else{ // if login is ok then we add a cookie 
@@ -48,7 +77,7 @@ while($info = mysqli_fetch_array( $check )){
 		//then redirect them to the members area 
 		header("Location: members.php"); 
 	}
-}
+}*/
 }
 else{
 // if they are not logged in 
---
+++ users.php
@@ -0,0 +1,22 @@
+<?php
+$users;
+$user1;
+$user2;
+$user1['user'] = 'user1';
+$user1['pass'] = '123456';
+$user1['name'] = "Usuario 1";
+$user2['user'] = 'user2';
+$user2['pass'] = '123456';
+$user2['name'] = "Usuario 2";
+$users[0] = $user1;
+$users[1] = $user2;
+
+function getUser($userToFindOut) {
+    foreach ($users as $key => $value) {
+        if($value['user'] == $userToFindOut) {
+            return $value;
+        }
+    }
+    return 0;
+}
+?>
